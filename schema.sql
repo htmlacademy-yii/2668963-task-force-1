63d99e7a575e05230347018bbaf5d5be4c96d07f
George <23459876-a@mail.ru>
2025-11-22 03:34:49 +0400

    Поправил схему БД

--- schema.sql
+++ schema.sql
@@ -42,11 +42,42 @@ CREATE TABLE tasks (
     location VARCHAR(128) NOT NULL,
     budget INT NOT NULL,
     deadline DATETIME NOT NULL,
-    file VARCHAR(255) NOT NULL,
+    status VARCHAR(128),
     customer_id INT NOT NULL,
     performer_id INT,
     FOREIGN KEY (customer_id) REFERENCES users(id),
     FOREIGN KEY (performer_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id),
     INDEX task_title (title)
+);
+
+CREATE TABLE files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task_id INT,
+    file VARCHAR(128) NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
+);
+
+CREATE TABLE reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR(128) NOT NULL,
+    score INT NOT NULL,
+    task_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    performer_id INT NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (customer_id) REFERENCES users(id),
+    FOREIGN KEY (performer_id) REFERENCES users(id)
+);
+
+CREATE TABLE offers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    performer_id INT NOT NULL,
+    price INT NOT NULL,
+    task_id INT NOT NULL,
+    status VARCHAR(128) NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (performer_id) REFERENCES users(id)
 );
